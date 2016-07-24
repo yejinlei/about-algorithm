b2b6c6ac4487b1ea882add79520a93ba15e0d124
4010896@qq.com <4010896@qq.com>
2016-07-24 19:51:33 +0800

    manhattan distance

--- basic/distance.py
+++ basic/distance.py
@@ -4,13 +4,27 @@ import math
 def dis_euclidean(A, B):
     """euclidean distance
     """
-    res = -1
-    if len(A) != len(B):    return res
+    if len(A) != len(B):
+        return -1
 
-    for ai in A:
-        for bi in B:
-            res += (ai-bi)*(ai-bi)
+    res = 0
+    length = len(A)
+    for i in xrange(length):
+        res += (A[i]-B[i])*(A[i]-B[i])
     return math.sqrt(res)
 
+def dis_manhattan(A, B):
+    """manhattan distance
+    """
+    if len(A) != len(B):
+        return -1
+
+    res = 0
+    length = len(A)
+    for i in xrange(length):
+        res += math.fabs(A[i]-B[i])
+    return res
+
 if __name__ == "__main__":
-    print dis_euclidean([1,2,3],[1,2,3])
+    print dis_euclidean([1,2,3],[4,5,6])
+    print dis_manhattan([1,2,3],[4,5,6])
